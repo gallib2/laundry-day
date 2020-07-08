d3e7cf3cf1d01bdd57c7f72e9430cbf42252ae50
Semimartian <semimartian@hotmail.com>
2020-07-08 04:32:00 +0300

    Added a new spawning system that allows us to create walls of clothing items instead of spawning them one at a time. Added some place-holder UI, changed the system that controls clothing type required so that it knows when the next change happens in advance, thus we can warn about it. Also, the next clothing type required is known to the player and is shown in UI.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,62 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    private static ClothingType clothingTypeRequired;
-    public static ClothingType ClothingTypeRequired
+    private static ClothingType currentClothingTypeRequired;
+    public static ClothingType CurrentClothingTypeRequired
     {
-        get { return clothingTypeRequired; }
+        get { return currentClothingTypeRequired; }
     }
+    private static ClothingType nextClothingTypeRequired;
+    public static event Action<ClothingType, ClothingType> OnClothingTypeRequiredChanged;
+
     [SerializeField] private float minimumSecondsBetweenClothingTypeChanges;
     [SerializeField] private float maximumSecondsBetweenClothingTypeChanges;
+    private float nextClothingTypeChangeScheduled;
+    public static event Action<float> OnClothingTypeChangeWarning;
+
+    [SerializeField] private int warningOfClothingTypeChangeDuration;
+
 
     private void Start()
     {
-        ChangeClothingTypeRequired();
+        Initialise();
+    }
+
+    private void Initialise()
+    {
+        InitialiseClothingTypeRequired();
+    }
+
+    private void DetermineNextClothingTypeChangeSchedule()
+    {
+        nextClothingTypeChangeScheduled = Time.time + 
+            (UnityEngine.Random.Range(minimumSecondsBetweenClothingTypeChanges, maximumSecondsBetweenClothingTypeChanges));
+    }
+
+    private void InitialiseClothingTypeRequired()
+    {
+        currentClothingTypeRequired = (ClothingType)
+            UnityEngine.Random.Range(0, (int)ClothingType.LENGTH);
+
+        bool newColthingTypeIsDifferentToOldOne = false;
+        while (!newColthingTypeIsDifferentToOldOne)
+        {
+            ClothingType newClothingType = (ClothingType)
+               UnityEngine.Random.Range(0, (int)ClothingType.LENGTH);
+            if (newClothingType != currentClothingTypeRequired)
+            {
+                nextClothingTypeRequired = newClothingType;
+                newColthingTypeIsDifferentToOldOne = true;
+            }
+        }
+
+        DetermineNextClothingTypeChangeSchedule();
+
+        OnClothingTypeRequiredChanged(currentClothingTypeRequired, nextClothingTypeRequired);
     }
 
     private void ChangeClothingTypeRequired()
@@ -23,18 +65,32 @@ public class GameManager : Singleton<GameManager>
         while (!newColthingTypeIsDifferentToOldOne)
         {
             ClothingType newClothingType = (ClothingType)
-            Random.Range(0, (int)ClothingType.LENGTH);
-            if(newClothingType != clothingTypeRequired)
+               UnityEngine.Random.Range(0, (int)ClothingType.LENGTH);
+            if(newClothingType != nextClothingTypeRequired)
             {
-                clothingTypeRequired = newClothingType;
-                InformationText.Instance.UpdateText(newClothingType.ToString());
+                currentClothingTypeRequired = nextClothingTypeRequired;
+                nextClothingTypeRequired = newClothingType;
                 newColthingTypeIsDifferentToOldOne = true;
             }
         }
         
-        Invoke("ChangeClothingTypeRequired",
-            Random.Range(minimumSecondsBetweenClothingTypeChanges, maximumSecondsBetweenClothingTypeChanges));
+       /* Invoke("ChangeClothingTypeRequired",
+            Random.Range(minimumSecondsBetweenClothingTypeChanges, maximumSecondsBetweenClothingTypeChanges));*/
+        OnClothingTypeRequiredChanged(currentClothingTypeRequired, nextClothingTypeRequired);
+    }
 
-        Debug.Log("ClothingTypeRequired:" + ClothingTypeRequired.ToString()) ;
+    private void Update()
+    {
+        float time = Time.time;
+        float timeDifference = nextClothingTypeChangeScheduled - time;
+        if (timeDifference < warningOfClothingTypeChangeDuration)
+        {
+            OnClothingTypeChangeWarning(timeDifference);
+        }
+        if (time >= nextClothingTypeChangeScheduled)
+        {
+            ChangeClothingTypeRequired();
+            DetermineNextClothingTypeChangeSchedule();
+        }
     }
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : Singleton<UIManager>
+{
+    [SerializeField] private TMPro.TextMeshProUGUI mileageText;
+    [SerializeField] private TMPro.TextMeshProUGUI livesText;
+    [SerializeField] private TMPro.TextMeshProUGUI washedItemsText;
+    [SerializeField] private TMPro.TextMeshProUGUI clothingTypeChangeWarningText;
+    [SerializeField] private Image currentClothingTypeRequiredImage;
+    [SerializeField] private Image nextClothingTypeRequiredImage;
+
+    [SerializeField] private Sprite[] currentClothingTypeRequiredSprites;
+    [SerializeField] private Sprite[] nextClothingTypeRequiredSprites;
+
+
+    private void OnEnable()
+    {
+        Player.OnMileageChanged += UpdateMileageText;
+        Player.OnLivesChanged += UpdateLivesText;
+        Player.OnWashedItemsChanged += UpdateWashedItemsText;
+        GameManager.OnClothingTypeRequiredChanged += UpdateClothingTypeRequired;
+        GameManager.OnClothingTypeChangeWarning += UpdateClothingTypeChangeWarningText;
+
+    }
+
+    private void OnDisable()
+    {
+        Player.OnMileageChanged -= UpdateMileageText;
+        Player.OnLivesChanged -= UpdateLivesText;
+        Player.OnWashedItemsChanged -= UpdateWashedItemsText;
+        GameManager.OnClothingTypeRequiredChanged -= UpdateClothingTypeRequired;
+        GameManager.OnClothingTypeChangeWarning -= UpdateClothingTypeChangeWarningText;
+
+    }
+
+    private void UpdateMileageText(System.UInt32 mileage)
+    {
+        mileageText.text = mileage.ToString();
+    }
+
+    private void UpdateLivesText(int lives)
+    {
+        livesText.text = lives.ToString();
+    }
+
+    private void UpdateWashedItemsText(System.UInt32 washedItems)
+    {
+        washedItemsText.text = washedItems.ToString();
+    }
+
+    private void UpdateClothingTypeRequired
+        (ClothingType currentClothingTypeRequired, ClothingType nextClothingTypeRequired)
+    {
+        currentClothingTypeRequiredImage.sprite = 
+            currentClothingTypeRequiredSprites[(int)currentClothingTypeRequired];
+        nextClothingTypeRequiredImage.sprite =
+            nextClothingTypeRequiredSprites[(int)nextClothingTypeRequired];
+
+        clothingTypeChangeWarningText.text = "";
+
+    }
+
+    private void UpdateClothingTypeChangeWarningText(float time)
+    {
+        clothingTypeChangeWarningText.text = time.ToString("f1");
+    }
+}
