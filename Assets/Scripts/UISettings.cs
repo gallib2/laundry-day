a5649b2112a7a3cdf36aa1410997e4a8a2705cec
Semimartian <semimartian@hotmail.com>
2020-07-11 06:05:35 +0300

    Added new camera options, Played with platforms scale, Added new feedbacks (a new bubble bursting particle system, new animtion..), added a customisable X speed, added a boolian (available in settings) for controlling whether to allow or forbid switching lanes mid-air

--- Assets/Scripts/UISettings.cs
+++ Assets/Scripts/UISettings.cs
@@ -6,21 +6,24 @@ using UnityEngine.UI;
 public class UISettings : MonoBehaviour
 {
     [SerializeField]
-    private Slider sliderPlayerMinimumSpeed;
+    private Slider sliderPlayerMinimumZSpeed;
     [SerializeField]
-    private Text textPlayerMinimumSpeed;
+    private Text textPlayerMinimumZSpeed;
     [SerializeField]
-    private Slider sliderPlayerMaximumSpeed;
+    private Slider sliderPlayerMaximumZSpeed;
     [SerializeField]
-    private Text textPlayerMaximumSpeed;
+    private Text textPlayerMaximumZSpeed;
+    [SerializeField] private Slider playerXSpeedSlider;
+    [SerializeField] private Text playerXSpeedText;
     [SerializeField]
     private Slider sliderPlayerJumpForce;
     [SerializeField]
     private Text textPlayerJumpForce;
     [SerializeField]
     private Dropdown dropdownCameraPositionOptions;
+    [SerializeField] Toggle forbidSwitchingLanesWhileAirborneToggle;
 
-    [SerializeField] private Text livesAtStartText;
+ [SerializeField] private Text livesAtStartText;
     [SerializeField] private Slider livesAtStartSlider;
 
     public bool SetMinSpeed { get; set; }
@@ -28,8 +31,10 @@ public class UISettings : MonoBehaviour
 
     private void Start()
     {
-        textPlayerMinimumSpeed.text = sliderPlayerMinimumSpeed.value.ToString();
-        textPlayerMaximumSpeed.text = sliderPlayerMaximumSpeed.value.ToString();
+        textPlayerMinimumZSpeed.text = sliderPlayerMinimumZSpeed.value.ToString();
+        textPlayerMaximumZSpeed.text = sliderPlayerMaximumZSpeed.value.ToString();
+        playerXSpeedText.text = playerXSpeedSlider.value.ToString();
+
         textPlayerJumpForce.text = sliderPlayerJumpForce.value.ToString();
         livesAtStartText.text = livesAtStartSlider.value.ToString();
 
@@ -42,19 +47,22 @@ public class UISettings : MonoBehaviour
 
     public void SetChanges()
     {
-        bool canSetSpeedValues = sliderPlayerMinimumSpeed.value <= sliderPlayerMaximumSpeed.value;
+        bool canSetSpeedValues = sliderPlayerMinimumZSpeed.value <= sliderPlayerMaximumZSpeed.value;
         
         if(canSetSpeedValues)
         {
-            Settings.Instance.PlayerMinimumSpeed = (int)sliderPlayerMinimumSpeed.value;
-            Settings.Instance.PlayerMaximumSpeed = (int)sliderPlayerMaximumSpeed.value;
+            Settings.Instance.PlayerMinimumSpeed = (int)sliderPlayerMinimumZSpeed.value;
+            Settings.Instance.PlayerMaximumSpeed = (int)sliderPlayerMaximumZSpeed.value;
+
         }
         else
         {
             // min value cant be bigger then the max. so if min < max the min is set for both
-            Settings.Instance.PlayerMinimumSpeed = (int)sliderPlayerMinimumSpeed.value;
-            Settings.Instance.PlayerMaximumSpeed = (int)sliderPlayerMinimumSpeed.value;
+            Settings.Instance.PlayerMinimumSpeed = (int)sliderPlayerMinimumZSpeed.value;
+            Settings.Instance.PlayerMaximumSpeed = (int)sliderPlayerMinimumZSpeed.value;
         }
+
+        Settings.Instance.PlayerXSpeed = (int)playerXSpeedSlider.value;
 
         int index = dropdownCameraPositionOptions.value;
         Settings.Instance.ChosenCameraOption = Settings.Instance.CameraOptions[index];
@@ -62,6 +70,13 @@ public class UISettings : MonoBehaviour
 
         Settings.Instance.PlayeJumpForce = sliderPlayerJumpForce.value;
         Settings.Instance.LivesAtStart = (int)livesAtStartSlider.value;
+
+        Settings.Instance.ForbidSwitchingLanesWhileAirborne = forbidSwitchingLanesWhileAirborneToggle.isOn;
+    }
+
+    public void SetForbidSwitchingLanesWhileAirborne()
+    {
+        Settings.Instance.ForbidSwitchingLanesWhileAirborneIsSet = true;
     }
 
     public void SetLivesAtStart()
@@ -72,14 +87,20 @@ public class UISettings : MonoBehaviour
 
     public void SetPlayerMinimumSpeed()
     {
-        textPlayerMinimumSpeed.text = sliderPlayerMinimumSpeed.value.ToString();
+        textPlayerMinimumZSpeed.text = sliderPlayerMinimumZSpeed.value.ToString();
         Settings.Instance.SetMinSpeed = true;
     }
 
     public void SetPlayerMaximumSpeed()
     {
-        textPlayerMaximumSpeed.text = sliderPlayerMaximumSpeed.value.ToString();
+        textPlayerMaximumZSpeed.text = sliderPlayerMaximumZSpeed.value.ToString();
         Settings.Instance.SetMaxSpeed = true;
+    }
+
+    public void SetPlayerXSpeed()
+    {
+        playerXSpeedText.text = playerXSpeedSlider.value.ToString();
+        Settings.Instance.PlayerXSpeedIsSet = true;
     }
 
     public void SetPlayerJumpForce()
