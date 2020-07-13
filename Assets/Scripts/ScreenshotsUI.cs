699b7f725f37713f4508591a6a9a20eb16f3fb1d
Gal <gallib2@gmail.com>
2020-07-13 23:36:33 +0300

    add camera options images

---
+++ Assets/Scripts/ScreenshotsUI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenshotsUI : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    [SerializeField] private Sprite[] imgsScreenshots;
+    [SerializeField] private Dropdown dropdownCameraPositionOptions;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+        SetImageWithDropDownValue();
+    }
+
+    public void SetImageWithDropDownValue()
+    {
+        int index = dropdownCameraPositionOptions.value;
+        bool isValid = index >= 0 && index < imgsScreenshots.Length;
+        if (isValid)
+        {
+            image.sprite = imgsScreenshots[index];
+        }
+    }
+}
